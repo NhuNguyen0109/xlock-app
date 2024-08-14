39ab31cf0fbf2fc8dbd11dfb6aa2aefe2297444b
NhuNguyen0109 <nhu.nguyenquynh0109@hcmut.edu.vn>
2024-08-14 23:57:00 +0700

    feat: search-item

--- src/components/home-components/item-list/ItemList.tsx
+++ src/components/home-components/item-list/ItemList.tsx
@@ -1,12 +1,13 @@
-import SearchBar from "../../search-bar/SearchBar.tsx";
-import ItemCard from "./ItemCard";
-import { useDispatch } from "react-redux";
-import { useEffect } from "react";
+import { useState, useMemo, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { useQuery } from "@tanstack/react-query";
 import { itemActions } from "../../../store/item.slice.ts";
 import { apiCall, ApiEndpoints } from "../../../utils/index.ts";
+import { RootState } from "../../../store/index.ts";
+import "../../../assets/styles";
 import addOrderToAccounts from "../../../utils/orderAccount.ts";
 import AccountType from "../../../types/item.ts";
+import ItemCard from "./ItemCard";
 
 let DATA: AccountType[] = [
   {
@@ -35,8 +36,10 @@ let DATA: AccountType[] = [
 
 const ItemList = () => {
   const dispatch = useDispatch();
-
-  let itemsToRender = addOrderToAccounts(DATA);
+  const [searchQuery, setSearchQuery] = useState<string>("");
+  const selectedItem = useSelector(
+    (state: RootState) => state.item.selectedItem
+  );
 
   const { data, isSuccess, isError } = useQuery({
     queryKey: ["items"],
@@ -49,6 +52,8 @@ const ItemList = () => {
     },
   });
 
+  let itemsToRender = addOrderToAccounts(DATA);
+
   if (isSuccess) {
     itemsToRender = addOrderToAccounts(data);
   }
@@ -57,27 +62,48 @@ const ItemList = () => {
     console.log("Error in fetching items");
   }
 
+  // Filter items based on the search query
+  const filteredItems = useMemo(() => {
+    return searchQuery.trim()
+      ? itemsToRender.filter(
+          (item) =>
+            item.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
+            item.site.toLowerCase().includes(searchQuery.toLowerCase())
+        )
+      : itemsToRender;
+  }, [searchQuery, itemsToRender]);
+
   useEffect(() => {
-    // if (data.length > 0) {
-    //   dispatch(itemActions.selectItem(data[0]));
-    // } else {
-    //   dispatch(itemActions.selectItem(DATA[0]));
-    // }
     dispatch(itemActions.selectItem(itemsToRender[0]));
   }, [dispatch]);
+
+  useEffect(() => {
+    setSearchQuery("");
+  }, [selectedItem]);
 
   return (
     <div className="item-list-section flex flex-col flex-grow">
       <div className="header w-full h-[48px] p-[12px] bg-[#E6F1FD] flex items-center justify-between">
-        <SearchBar data={itemsToRender} />
+        <div className="w-full mr-[16px]">
+          <input
+            type="text"
+            placeholder="Search"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            className="h-[24px] w-full rounded-[12px] px-[12px] inactive-title focus:outline-[#0570EB] body-text"
+          />
+        </div>
         <div className="w-[24px] h-[24px] flex justify-center items-center">
           <img src="/images/New.png" alt="More" className="" />
         </div>
       </div>
-      <div className="access-history-list w-full h-full bg-white flex flex-col gap-[6px] mt-[6px]">
-        {itemsToRender.map((item) => (
+      <div className="access-history-list w-full h-full bg-white flex flex-col gap-[6px] mt-[6px] relative">
+        {filteredItems.map((item) => (
           <ItemCard key={item.id} {...item} />
         ))}
+        {filteredItems.length === 0 && (
+          <p className="body-text-title text-center">No items found</p>
+        )}
       </div>
     </div>
   );
